d2f441919b0e4ae0af0d72b7f51990a620b9e4fa
domnatr09 <65861188+domnatr09@users.noreply.github.com>
2022-01-21 20:47:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-pass
+usage = """
+pkg
+
+Usage:
+  pkg -h | --help
+  pkg -v | --version
+
+Options:
+  -h --help     Show this screen.
+  -v --version     Show the current version.
+"""
+from docopt import docopt
+
+args=docopt(usage)
+
+if args["--version"] == True:
+  print("pkg version 0.1)
